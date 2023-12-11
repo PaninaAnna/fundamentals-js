8ceae839de11f2ec7af0a30eadd1edb2c64999f6
PaninaAnna <150665004+PaninaAnna@users.noreply.github.com>
2023-12-11 18:02:29 +0000

    add index

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+export const concatStrings = (a , b) => a + b;
 
+export const isString = (value) => typeof value === 'string';
+
+export const identifySign = (num) => {
+    if (num === 0) {
+        return 'Ноль';
+    } if (num > 0) {
+        return 'Положительное число'
+    } if (num < 0) {
+        return 'Отрицательное число'
+    }
+}
+
+export const reverseWords = (text) => text.split(' ').reverse().join(' ');
+
+export const wordsCount =(text) => {
+    return text.split(' ').length;
+}
